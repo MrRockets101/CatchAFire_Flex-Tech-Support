e415d0428c1a358176bb223642b971c65803115f
alex brosovich <alexbrosovich@gmail.com>
2026-01-14 15:21:18 -0500

    Reorganize project structure: move frontend files to frontend/ directory
    - Moved components/, contexts/, hooks/, utils/ from root to frontend/
    - Moved App.js, app.json, index.js, modules.js to frontend/
    - Moved assets/ to frontend/
    - Updated all import paths in App.js to work with new structure
    - Cleaned up duplicate files from root directory
    - Updated README.md with correct project structure

--- components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as CurrencyModal } from "./CurrencyModal";
-export { default as DeliveryModal } from "./DeliveryModal";
-export { default as DeliveryConfirmationModal } from "./DeliveryConfirmationModal";
-export { default as OrderForm } from "./OrderForm";
--- contexts/index.js
+++
@@ -1,5 +0,0 @@
-// Context exports for easy importing
-export { CurrencyProvider, useCurrency } from "./CurrencyContext";
-export { OrderProvider, useOrder } from "./OrderContext";
-export { LocationProvider, useLocation } from "./LocationContext";
-export { UserProvider, useUser } from "./UserContext";
